3b64e08f792b1a87f1a7c36fb099c1859e655090
Robert Tairas <robert.tairas@vanderbilt.edu>
2019-12-06 00:18:17 -0600

    Feedback branch for glennca1 (githubid: UCGv2) generated by squashing tree-ish at e1e5cc7b6f623a113595a0338c9da265e0337b1a onto branch pointer at refs/remotes/origin/master

--- src/main.rb
+++ src/main.rb
@@ -1,17 +1,29 @@
-# Name:
-# VUnetID:
-# Email:
+# Name: Christopher Glenn
+# VUnetID: glennca1
+# Email: christopher.a.glenn@vanderbilt.edu
 # Class: CS 3270 - Vanderbilt University
-# Date:
-# Honor Statement:
+# Date: 12/05/19
+# Honor Statement: I have neither given nor received unauthorized help on this assignment
 
-# Description:
+# Description: This is a sudoku solver implemented in Ruby
 
 require_relative "sudoku"
 
 puzzle = Sudoku.new
 
-# puzzle.loadFromFile("txt/sudoku-test1.txt")
+puzzle.loadFromFile("txt/sudoku-test1.txt")
 
 print "\nHere is the initial board:\n\n"
 puzzle.printSudokuBoard
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+if puzzle.solve(0,0) then
+	ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+	elapsed = ending - starting
+	print "\nHere is the solved board!\n\n"
+	puzzle.printSudokuBoard
+	
+	print "\nPuzzle solved in " + elapsed.to_s + " seconds.\n"
+else
+	print "\nNo solution. \n\n"
+end
--- src/sudoku.rb
+++ src/sudoku.rb
@@ -1,14 +1,14 @@
 # CS 3270 instructor
 # Vanderbilt University
 
-# Name:
-# VUnetID:
-# Email:
+# Name: Christopher Glenn
+# VUnetID: glennca1
+# Email: christopher.a.glenn@vanderbilt.edu
 # Class: CS 3270 - Vanderbilt University
-# Date:
-# Honor Statement:
+# Date: 12/05/19
+# Honor Statement: I have neither given nor received unauthorized help on this assignment
 
-# Description:
+# Description: This is the declaration of a sudoku solver 
 
 class Sudoku
 
@@ -56,5 +56,130 @@ class Sudoku
             end
         end
     end
+	
+	# Load board from file
+	def loadFromFile(fileName)
+    inFile = File.new(fileName, "r")
+    index = 0
+
+    # Iterate over the lines from the file.
+    inFile.each_line do |line|
+        # line is a string containing one line from the file.
+
+        ### The code to process the line goes here. ###
+		vals = line.split
+		# The collect block will iterate over each item in vals (strings)
+		# and perform the function (convert to int) returning the set as a new array.
+		@sudokuBoard[index] = vals.collect { |x| x.to_i }
+        index = index + 1
+    end
+	
+	
+	# Returns whether a number can be placed in this column
+	#
+	# @param  col  A number representing the board column
+	# @param  num  A number we're checking
+	# @return Returns true if the number is valid in the column, false otherwise
+	def checkCol(col, num)
+		0.upto(BOARD_SIZE-1){
+		|row| 
+		if num == @sudokuBoard[row][col] then
+			return false
+		end
+		}
+		return true
+
+	end
+	# Returns whether a number can be placed in this row
+	#
+	# @param  row  A number representing the board row
+	# @param  num  A number we're checking
+	# @return Returns true if the number is valid in the row, false otherwise
+	def checkRow(row, num)
+		disjointSet = @sudokuBoard[row] - [num]
+		if disjointSet.length == 9 then
+			# Safe to add sum to the row.
+			return true
+		else
+			# Unsafe to add sum to the row, since it already existed there.
+			return false
+		end
+		
+	end
+	
+	# Returns whether a number can be placed in this subgrid
+	#
+	# @param  row  A number representing the board row
+	# @param  col  A number representing the board column
+	# @param  num  A number we're checking
+	# @return Returns true if the number is valid in the subgrid, false otherwise
+	def checkSubgrid(row, col, num)
+		x = (row / 3) * 3
+		y = (col / 3) * 3
+
+		box = @sudokuBoard[x][y..(y+2)] + @sudokuBoard[x+1][y..(y+2)] + @sudokuBoard[x+2][y..(y+2)]
+		disjointSet = box - [num]
+		if disjointSet.length == 9 then
+			# Safe to add num to the row.
+			return true
+		else
+			# Unsafe to add num to the row, since it already existed there.
+			return false
+		end
+		
+	
+	
+	
+	end
+	
+	# Returns whether a number can be placed per sudoku rules
+	#
+	# @param  row  A number representing the board row
+	# @param  col  A number representing the board column
+	# @param  num  A number we're checking
+	# @return Returns true if the number is valid in this postion, false otherwise
+	def check(row, col, num)
+		return checkCol(col,num) && checkRow(row,num) && checkSubgrid(row,col,num)
+	
+	end
+	
+	# A recursive backtracking function to solve sudoku boards 
+	#
+	# @param  row  A number representing the board row
+	# @param  col  A number representing the board column
+	# @return Returns true if the board is solved, false otherwise
+	def solve(row, col)
+	if row == 9 then
+		return true
+	end
+	
+	if col == 9 then
+		return solve(row +1, 0)
+	end
+	
+	if @sudokuBoard[row][col] != 0 then
+		return solve(row, col +1)
+	end
+	
+	1.upto(9) {
+	|num|
+	
+	if check(row,col,num) then
+		@sudokuBoard[row][col] = num
+		if solve(row, col+1) then
+			return true
+		end
+	end
+	}
+	
+	@sudokuBoard[row][col] = 0
+	return false
+	
+	
+	end
+		
+
+    inFile.close
+end
 
 end
